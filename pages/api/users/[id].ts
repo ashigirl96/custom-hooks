c9a1a058779805dc32a1d50df623273291dece24
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2021-06-22 13:10:14 +0900

    Impl: fetch user example

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -3,11 +3,18 @@ import { sampleUserData } from '../../../utils/sample-data'
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    if (!Array.isArray(sampleUserData)) {
-      throw new Error('Cannot find user data')
+    const {
+      query: { id }
+    } = req;
+    if (Array.isArray(id)) {
+        throw new Error('Cannot find user data')
     }
 
-    res.status(200).json(sampleUserData)
+    const data = sampleUserData.find((user) => user.id === +id);
+    if (!data) {
+      throw new Error('Cannot find user data')
+    }
+    res.status(200).json(data)
   } catch (err) {
     res.status(500).json({ statusCode: 500, message: err.message })
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,57 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { VFC } from "react";
+import axios, { AxiosRequestConfig } from "axios";
+import useSWR from "swr";
+import { User } from "../interfaces";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
 
-export default IndexPage
+export const axiosInstance = axios.create({
+  headers: { "X-Requested-With": "XMLHttpRequest" },
+});
+
+function request<ResponseBody = unknown>(object: AxiosRequestConfig) {
+  return axiosInstance.request<ResponseBody>(object);
+}
+
+function getUsersShow(id: string) {
+  return request<User>({
+    method: "GET",
+    url: `/api/users/${id}`
+  })
+}
+
+function useUser(id: number) {
+  const { data, error } = useSWR([id], getUsersShow);
+
+  return {
+    user: data?.data,
+    isLoading: !error && !data,
+    isError: error,
+    message: error?.message
+  };
+}
+
+type Props = {};
+const Index: VFC<Props> = ({}) => {
+  const id = 1010;
+
+  const { user, isLoading, isError, message } = useUser(id);
+
+  if (isLoading) {
+    return <div>is Loading...</div>
+  }
+
+  if (isError) {
+    return <div>is Error... {message}</div>
+  }
+
+
+  return (
+    <div>
+      <div>
+        hello ID {user.id} {user.name}
+      </div>
+    </div>
+  );
+};
+
+export default Index;
